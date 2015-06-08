e65cf7dd0377462dd4c14caf804f9c56e9101761
Ayush Goel <ayushgoel111@gmail.com>
2015-06-08 17:14:08 +0530

    Add script to install programs from lists

---
+++ install-program-list.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+# Install brew and gems as listed in `brew` and `gems` folder.
+
+require 'rake'
+
+def installFrom(filePath)
+    File.foreach(filePath) { |line|
+        name = line.chomp
+        yeild name
+    }
+end
+
+brewListPath = "brew" + File::SEPARATOR + "brew-list"
+installFrom(brewListPath) {|i| sh "brew install #{i}"}
+
+brewCaskListPath = "brew" + File::SEPARATOR + "brew-cask-list"
+installFrom(brewCaskListPath) {|i| sh "brew cask install #{i}"}
+
+gemListPath = "gems" + File::SEPARATOR + "gem-list"
+installFrom(gemListPath) {|i| sh "gem install #{i}"}
