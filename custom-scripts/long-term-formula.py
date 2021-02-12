3c5c28f598738be2457948aa8872196604496c73
Ayush Goel <ayushgoel111@gmail.com>
2021-02-12 20:06:31 +0530

    Add script to generate sheets formula for personal usage

---
+++ custom-scripts/long-term-formula.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+import argparse
+
+def generate_rows(lower, upper):
+  assert (lower <= upper), "Lower limit can not be larger than upper"
+  for i in range(lower, upper):
+    print("=VLOOKUP(C{0},MFPortfolio!G52:MFPortfolio!H72,2,FALSE)".format(i))
+
+def main():
+  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser.add_argument('upper', type=int, help='Upper limit to get rows')
+  parser.add_argument('--lower', type=int, default=3, help='Lower limit, defaults to 3')
+
+  args = parser.parse_args()
+  generate_rows(args.lower, args.upper)
+
+main()
