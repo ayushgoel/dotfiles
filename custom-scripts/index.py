9234d07ea6905d3eaef3839fa423082e0c02ec77
Ayush Goel <ayushgoel111@gmail.com>
2023-05-17 11:09:41 +0530

    Add index script

---
+++ custom-scripts/index.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+import argparse
+
+def generate_rows(lower, upper):
+  assert (lower <= upper), "Lower limit can not be larger than upper"
+  for i in range(lower, upper):
+    print("=VLOOKUP(C{0},MFPortfolio!G56:MFPortfolio!I76,3,FALSE)".format(i))
+
+def main():
+  parser = argparse.ArgumentParser(description='Process some integers.')
+  parser.add_argument('upper', type=int, help='Upper limit to get rows')
+  parser.add_argument('--lower', type=int, default=3, help='Lower limit, defaults to 3')
+
+  args = parser.parse_args()
+  generate_rows(args.lower, args.upper)
+
+main()
