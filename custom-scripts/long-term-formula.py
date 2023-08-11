0ef9c629db3186c5cd7eebe252b70e0c24e8f1f0
Ayush Goel <ayushgoel111@gmail.com>
2023-08-11 09:08:48 +0530

    Update long term script

--- custom-scripts/long-term-formula.py
+++ custom-scripts/long-term-formula.py
@@ -10,7 +10,7 @@ def generate_rows_S(lower, upper):
 def generate_rows_U(lower, upper):
   assert (lower <= upper), "Lower limit can not be larger than upper"
   for i in range(lower, upper):
-    print("=VLOOKUP(C{0},MFPortfolio!G58:MFPortfolio!H78,3,FALSE)".format(i))
+    print("=VLOOKUP(C{0},MFPortfolio!G58:MFPortfolio!I78,3,FALSE)".format(i))
 
 def main():
   parser = argparse.ArgumentParser(description='Process some integers.')
@@ -18,8 +18,10 @@ def main():
   parser.add_argument('--lower', type=int, default=3, help='Lower limit, defaults to 3')
 
   args = parser.parse_args()
+  print("Column S")
   generate_rows_S(args.lower, args.upper)
   print("\n")
+  print("Column U")
   generate_rows_U(args.lower, args.upper)
 
 main()
